7d6125e46eda584865d229529829bc9fbb9a06ca
Angelo Pirola <angelo9871@gmail.com>
2023-03-25 21:30:37 +0100

    Fix namespace

--- src/NET6CustomLibrary/EFCore/Infrastructure/Interfaces/IDatabaseRepository.cs
+++ src/NET6CustomLibrary/EFCore/Infrastructure/Interfaces/IDatabaseRepository.cs
@@ -1,4 +1,4 @@
-﻿namespace EFCoreGeneric.Infrastructure.Interfaces;
+﻿namespace NET6CustomLibrary.EFCore.Infrastructure.Interfaces;
 
 public interface IDatabaseRepository<TEntity, TKey> : IDatabase<TEntity, TKey> where TEntity : class, IEntity<TKey>, new()
 {
