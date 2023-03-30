4757a04da467fd93a928cce2c0a2ec820f9635aa
Angelo Pirola <angelo9871@gmail.com>
2023-03-30 10:54:10 +0200

    Refactoring DateTimeOnly

--- src/NET6CustomLibrary/DateTime/Converters/DateOnlyConverter.cs
+++ src/NET6CustomLibrary/DateTime/Converters/DateOnlyConverter.cs
@@ -1,22 +1,22 @@
 namespace NET6CustomLibrary.DateTime.Converters;
 
-//public class DateOnlyConverter : JsonConverter<DateOnly>
-//{
-//    public override DateOnly Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
-//    {
-//        var value = reader.GetString();
-//        return DateOnly.Parse(value);
-//    }
-
-//    public override void Write(Utf8JsonWriter writer, DateOnly value, JsonSerializerOptions options)
-//        => writer.WriteStringValue(value.ToString("yyyy-MM-dd"));
-//}
-
-public class DateOnlyConverter : ValueConverter<DateOnly, System.DateTime>
+public class DateOnlyConverter : JsonConverter<DateOnly>
 {
-    public DateOnlyConverter() : base(
-            dateOnly => dateOnly.ToDateTime(TimeOnly.MinValue),
-            dateTime => DateOnly.FromDateTime(dateTime))
+    public override DateOnly Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
     {
+        var value = reader.GetString();
+        return DateOnly.Parse(value);
     }
+
+    public override void Write(Utf8JsonWriter writer, DateOnly value, JsonSerializerOptions options)
+        => writer.WriteStringValue(value.ToString("yyyy-MM-dd"));
 }
+
+//public class DateOnlyConverter : ValueConverter<DateOnly, System.DateTime>
+//{
+//    public DateOnlyConverter() : base(
+//            dateOnly => dateOnly.ToDateTime(TimeOnly.MinValue),
+//            dateTime => DateOnly.FromDateTime(dateTime))
+//    {
+//    }
+//}
--- src/NET6CustomLibrary/Extensions/DependencyInjection.cs
+++ src/NET6CustomLibrary/Extensions/DependencyInjection.cs
@@ -59,8 +59,8 @@ public static class DependencyInjection
         builder.AddJsonOptions(options =>
         {
             options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
-            //options.JsonSerializerOptions.Converters.Add(new DateOnlyConverter());
-            //options.JsonSerializerOptions.Converters.Add(new TimeOnlyConverter());
+            options.JsonSerializerOptions.Converters.Add(new DateOnlyConverter());
+            options.JsonSerializerOptions.Converters.Add(new TimeOnlyConverter());
             options.JsonSerializerOptions.Converters.Add(new UtcDateTimeConverter());
         });
 
