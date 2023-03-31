2c1680ec7ae527426375ecad12fe7a1e33b3e61e
Angelo Pirola <angelo9871@gmail.com>
2023-04-01 00:23:38 +0200

    Aggiunta documentazione Swagger minima

---
+++ src/NET6CustomLibrary/Swagger/SwaggerSimple.cs
@@ -0,0 +1,29 @@
+﻿namespace NET6CustomLibrary.Swagger;
+
+public static class SwaggerSimple
+{
+    public static IServiceCollection AddSwaggerGenConfig(this IServiceCollection services, string title,
+        string version, string description = "", bool extendSchema = false, string xmlCommentsPath = "")
+    {
+        services
+            .AddEndpointsApiExplorer()
+            .AddSwaggerGen(options =>
+            {
+                options.OperationFilter<CultureAwareOperationFilter>();
+                options.SwaggerDoc($"{version}", new OpenApiInfo
+                {
+                    Title = $"{title}",
+                    Version = $"{version}",
+                    Description = $"{description}",
+                });
+
+                if (extendSchema)
+                    options.UseAllOfToExtendReferenceSchemas();
+
+                if (xmlCommentsPath is not (null or ""))
+                    options.IncludeXmlComments(xmlCommentsPath);
+            });
+
+        return services;
+    }
+}
