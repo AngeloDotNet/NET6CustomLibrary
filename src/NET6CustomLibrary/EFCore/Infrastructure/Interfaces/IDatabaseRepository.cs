ea4407c634a9eeebb0a46fddfc9e8d1d303e027c
Angelo Pirola <angelo9871@gmail.com>
2023-04-14 14:40:09 +0200

    WIP: Aggiunti nuovi metodi EFCore

--- src/NET6CustomLibrary/EFCore/Infrastructure/Interfaces/IDatabaseRepository.cs
+++ src/NET6CustomLibrary/EFCore/Infrastructure/Interfaces/IDatabaseRepository.cs
@@ -2,4 +2,7 @@
 
 public interface IDatabaseRepository<TEntity, TKey> : IDatabase<TEntity, TKey> where TEntity : class, IEntity<TKey>, new()
 {
+    Task<List<TEntity>> GetOrderByIdAscendingAsync();
+    Task<List<TEntity>> GetOrderByIdDescendingAsync();
+    Task<int> GetCountAsync();
 }
--- src/NET6CustomLibrary/EFCore/Infrastructure/Repository/DatabaseRepository.cs
+++ src/NET6CustomLibrary/EFCore/Infrastructure/Repository/DatabaseRepository.cs
@@ -5,4 +5,26 @@ public class DatabaseRepository<TEntity, TKey> : Database<TEntity, TKey>, IDatab
     public DatabaseRepository(DbContext dbContext) : base(dbContext)
     {
     }
+
+    public async Task<int> GetCountAsync()
+    {
+        return await DbContext.Set<TEntity>()
+            .CountAsync();
+    }
+
+    public async Task<List<TEntity>> GetOrderByIdAscendingAsync()
+    {
+        return await DbContext.Set<TEntity>()
+            .OrderBy(x => x.Id)
+            .AsNoTracking()
+            .ToListAsync();
+    }
+
+    public async Task<List<TEntity>> GetOrderByIdDescendingAsync()
+    {
+        return await DbContext.Set<TEntity>()
+            .OrderByDescending(x => x.Id)
+            .AsNoTracking()
+            .ToListAsync();
+    }
 }
