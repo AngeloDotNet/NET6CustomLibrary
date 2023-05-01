415ba327c0b1be038bed09f5696925e8cb450aae
Angelo Pirola <angelo9871@gmail.com>
2023-05-01 16:04:50 +0200

    Aggiunti metodi per utilizzo cache tramite REDIS

---
+++ src/NET6CustomLibrary/RedisCache/ICacheService.cs
@@ -0,0 +1,7 @@
+﻿namespace NET6CustomLibrary.RedisCache;
+
+public interface ICacheService
+{
+    T Get<T>(string key);
+    T Set<T>(string key, T value, TimeSpan? absoluteExpireTime = null, TimeSpan? slidingExpireTime = null);
+}
