4224266edb240f985f158b3bfb017d46a7e9aa0a
Angelo Pirola <angelo9871@gmail.com>
2023-05-03 11:07:12 +0200

    Aggiunti metodi statici per Scrutor

---
+++ src/NET6CustomLibrary/Scrutor/ScrutorService.cs
@@ -0,0 +1,37 @@
+﻿namespace NET6CustomLibrary.Scrutor;
+
+public static class ScrutorService
+{
+    public static IServiceCollection AddRegisterTransientService<TAssembly>(this IServiceCollection services, string stringEndsWith) where TAssembly : class
+    {
+        services.Scan(scan =>
+            scan.FromAssemblyOf<TAssembly>()
+                .AddClasses(classes => classes.Where(type => type.Name.EndsWith(stringEndsWith)))
+                .AsImplementedInterfaces()
+                .WithTransientLifetime());
+
+        return services;
+    }
+
+    public static IServiceCollection AddRegisterSingletonService<TAssembly>(this IServiceCollection services, string stringEndsWith) where TAssembly : class
+    {
+        services.Scan(scan =>
+            scan.FromAssemblyOf<TAssembly>()
+                .AddClasses(classes => classes.Where(type => type.Name.EndsWith(stringEndsWith)))
+                .AsImplementedInterfaces()
+                .WithSingletonLifetime());
+
+        return services;
+    }
+
+    public static IServiceCollection AddRegisterScopedService<TAssembly>(this IServiceCollection services, string stringEndsWith) where TAssembly : class
+    {
+        services.Scan(scan =>
+            scan.FromAssemblyOf<TAssembly>()
+                .AddClasses(classes => classes.Where(type => type.Name.EndsWith(stringEndsWith)))
+                .AsImplementedInterfaces()
+                .WithScopedLifetime());
+
+        return services;
+    }
+}
