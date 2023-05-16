d9ec84f6a3d7e20b51a49827b6c952f107075a99
Angelo Pirola <angelo9871@gmail.com>
2023-05-17 00:41:21 +0200

    Aggiunta metodo statico ResultUnprocessableEntity

--- src/NET6CustomLibrary/Errors/ErrorResult.cs
+++ src/NET6CustomLibrary/Errors/ErrorResult.cs
@@ -7,4 +7,32 @@ public class ErrorResult
     public int TypeCode { get; set; } = 0;
     public string InstancePath { get; set; }
     public List<string> Message { get; set; }
+
+    public static ErrorResult ResultUnprocessableEntity(List<string> listErrors, HttpContext httpContext)
+    {
+        ErrorResult errorResult = new()
+        {
+            TitleCode = "Validation errors",
+            StatusCode = HttpStatusCode.UnprocessableEntity,
+            TypeCode = 0,
+            InstancePath = httpContext.Request.Path,
+            Message = listErrors
+        };
+
+        return errorResult;
+    }
+
+    //public static ErrorResult ResultNotFound(List<string> listErrors, HttpContext httpContext)
+    //{
+    //    ErrorResult errorResult = new()
+    //    {
+    //        TitleCode = "Not Found",
+    //        StatusCode = HttpStatusCode.NotFound,
+    //        TypeCode = 0,
+    //        InstancePath = httpContext.Request.Path,
+    //        Message = listErrors
+    //    };
+
+    //    return errorResult;
+    //}
 }
