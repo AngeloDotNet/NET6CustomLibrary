4a6b07cb04d1e08d3dc76fe9952829aca9bc44b9
Angelo Pirola <angelo9871@gmail.com>
2023-05-24 00:05:12 +0200

    Refactoring Response Result

---
+++ src/NET6CustomLibrary/Results/ResponseResult.cs
@@ -0,0 +1,24 @@
+﻿namespace NET6CustomLibrary.Results;
+
+public class ResponseResult
+{
+    public bool Succeeded { get; set; }
+    public HttpStatusCode StatusCodes { get; set; }
+    public int TypeCode { get; set; }
+    public object Message { get; set; }
+    public string InstancePath { get; set; }
+    public string Detail { get; set; }
+
+    internal ResponseResult(bool succeeded, int typeCode, HttpContext httpContext, object message, HttpStatusCode statusCodes)
+    {
+        Succeeded = succeeded;
+        StatusCodes = statusCodes;
+        TypeCode = typeCode;
+        InstancePath = httpContext.Request.Path;
+        Detail = $"https://httpstatuses.io/{(int)statusCodes}";
+        Message = message;
+    }
+
+    public static ResponseResult Result(bool succeeded, int typeCode, HttpContext httpContext, object message, HttpStatusCode statusCodes)
+        => new(succeeded, typeCode, httpContext, message, statusCodes);
+}
