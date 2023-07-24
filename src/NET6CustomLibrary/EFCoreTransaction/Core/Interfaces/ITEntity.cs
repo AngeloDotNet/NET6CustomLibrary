f3a45f4b162e76289d2d9cf4f642c63790100d2d
Angelo Pirola <angelo9871@gmail.com>
2023-07-24 09:07:43 +0200

    Refactoring

--- src/NET6CustomLibrary/EFCoreTransaction/Core/Interfaces/ITEntity.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace NET6CustomLibrary.EFCoreTransaction.Core.Interfaces;
-
-public interface ITEntity<TKey>
-{
-    TKey Id { get; set; }
-}
--- src/NET6CustomLibrary/EFCoreTransaction/Infrastructure/Interfaces/ITUnitOfWork.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace NET6CustomLibrary.EFCoreTransaction.Infrastructure.Interfaces;
-
-public interface ITUnitOfWork<TEntity, TKey> : IDisposable where TEntity : class, ITEntity<TKey>, new()
-{
-    IRepository<TEntity, TKey> Repository { get; }
-}
